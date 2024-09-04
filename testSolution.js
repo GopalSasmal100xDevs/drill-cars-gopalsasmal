b7d699950338d767f1fde9434cb88295c464b182
gopalprojectx100 <gopal.sasmal.29.3@mountblue.tech>
2024-09-04 21:18:55 +0530

    change file name

---
+++ testSolution.js
@@ -0,0 +1,7 @@
+import inventory from "./inventory.js";
+import { getCarId33 } from "./solution.js";
+
+console.log(getCarId33(inventory));
+// console.log(getCarId33());
+// console.log(getCarId33([{ id: 33 }]));
+// console.log(getCarId33());
