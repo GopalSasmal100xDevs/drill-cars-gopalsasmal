54e9f9fd686b2a7dc45c67f69feb5d85e62e922f
gopalprojectx100 <gopal.sasmal.29.3@mountblue.tech>
2024-09-04 21:32:17 +0530

    Added problem 2 solution

--- testSolution.js
+++ testSolution.js
@@ -1,7 +1,14 @@
 import inventory from "./inventory.js";
-import { getCarId33 } from "./solution.js";
+import { getCarId33, lastCarInInventory } from "./solution.js";
 
+// # Problem 1
 console.log(getCarId33(inventory));
 // console.log(getCarId33());
 // console.log(getCarId33([{ id: 33 }]));
 // console.log(getCarId33());
+
+// # Problem 2
+console.log(lastCarInInventory(inventory));
+// console.log(lastCarInInventory([{}, {}]));
+// console.log(lastCarInInventory());
+// console.log(lastCarInInventory([{}, { car_make: "BMW" }]));
